898fe9cc8f42a1fb1795fd26629b28c83eeebf67
danielwt1 <71347694+danielwt1@users.noreply.github.com>
2022-03-17 19:47:02 -0500

    FIRST COMMIT TASK-SPRING-EXAMPLE

---
+++ src/test/java/com/example/tasktable/TasktableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasktable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
